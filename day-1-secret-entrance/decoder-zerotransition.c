d43fe07222d348a0263172bd638bf5a28799bf10
Paul Goldschmidt <mail@paul-goldschmidt.de>
2025-12-02 09:18:16 +0800

    add day one pt 2 code

---
+++ day-1-secret-entrance/decoder-zerotransition.c
@@ -0,0 +1,31 @@
+// By Paul G, written in QR893 @ 33000ft / 2025-12-02
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *file = fopen("input.txt", "r");
+    int position = 50; //start
+    int zero_count = 0; //zero transition counter
+    char line[256]; //buffer
+    while (fgets(line, sizeof(line), file)) {
+        char direction = line[0];
+        int distance = atoi(line + 1);
+        int zeros_this_rotation; //zero transition counter this rotation
+        if (direction == 'R') {
+            zeros_this_rotation += (position + distance) / 100;
+        } else if (direction == 'L') {
+            zeros_this_rotation += (distance - position + 99) / 100;
+        }
+        zero_count += zeros_this_rotation;
+        if (direction == 'L') {
+            position = (position - distance % 100 + 100) % 100;
+        } else if (direction == 'R') {
+            position = (position + distance) % 100;
+        }
+    }
+    fclose(file);
+    printf("password pt. 2: %d\n", zero_count);
+    return 0;
+}
