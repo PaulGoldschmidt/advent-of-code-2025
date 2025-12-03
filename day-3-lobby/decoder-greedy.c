7b43db7ddf5de08a6245877225a9c28a66418592
Paul Goldschmidt <mail@paul-goldschmidt.de>
2025-12-03 11:15:44 +0100

    add greedy algo
    not correct yet tho 🥀

--- day-3-lobby/decoder-greedy.c
+++ day-3-lobby/decoder-greedy.c
@@ -4,6 +4,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+int NUM_DIGITS = 12;
+
+long long calc_joltage(const char *bank, int len) {
+    char result[NUM_DIGITS + 1];
+    int start = 0;
+    for (int i = 0; i < NUM_DIGITS; i++) {
+        int end = len - (NUM_DIGITS - i - 1);
+        int best_pos = start;
+        char best_digit = bank[start];
+        for (int j = start; j < end; j++) {
+            if (bank[j] > best_digit) {
+                best_digit = bank[j];
+                best_pos = j;
+            }
+        }
+        result[i] = best_digit;
+        start = best_pos + 1;
+    }
+    result[NUM_DIGITS] = '\0';
+
+    long long value = 0; //converter
+    for (int i = 0; i < NUM_DIGITS; i++) {
+        value = value * 10 + (result[i] - '0');
+    }
+
+    return value;
+    }
+
 
 int main(void) {
     char line[10000];
@@ -12,11 +40,11 @@ int main(void) {
 
     while (fgets(line, sizeof(line), f)) {
         int len = strlen(line);
-        int joltage = calc_joltage(line, len);
+        long long joltage = calc_joltage(line, len);
         total += joltage;
     }
     
-    printf("\nfinal sum: %llu\n", total);
+    printf("\nfinal sum pt. 2: %llu\n", total);
     fclose(f);
     return 0;
 }
